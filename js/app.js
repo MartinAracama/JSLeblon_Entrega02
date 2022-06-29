f795442e6034a5479bb48981160967e8c707f5b3
Martin Aracama <martinaracama@gmail.com>
2022-06-29 20:17:06 -0300

    Primer Commit

---
+++ js/app.js
@@ -0,0 +1,96 @@
+
+const carrito = [];
+
+
+// DOM de productos
+function renderizarProductos() {
+  let tienda = document.getElementById("tienda");
+
+  BASE.forEach((e) => {
+    let productoHTML = `
+
+        <div class="col-12 col-md-4 mb-5 d-flex justify-content-center">
+        <div class="card text-dark" style="width: 18rem;">
+            <img class="card-img-top" src="${e.img}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${e.nombre}</h5>
+                <p class="card-text">${e.descripcion}</p>
+                <p>$${e.precio}</p>
+                <button class="btn btn-primary" onClick="agregarProductoAlCarrito(${e.id})">Añadir al carrito</button>
+            </div>
+        </div>
+        </div>
+        `;
+    tienda.innerHTML += productoHTML;
+  });
+}
+
+renderizarProductos();
+
+
+// Funcion para agregar productos al carrito
+function agregarProductoAlCarrito(id) {
+  let producto = BASE.find((producto) => producto.id == id);
+
+  let productoEnCarrito = carrito.find((producto) => producto.id == id);
+
+  if (productoEnCarrito) {
+    productoEnCarrito.cantidad++;
+  } else {
+    producto.cantidad = 1;
+    carrito.push(producto);
+  }
+
+  renderizarCarrito();
+}
+
+function renderizarCarrito() {
+  let carritoHTML = document.getElementById("carrito");
+
+  html = "";
+
+  carrito.forEach((producto, id) => {
+    html += `
+        <div class="col-12 col-md-4 mb-5 d-flex justify-content-center">
+            <div class="card text-dark" style="width: 18rem;">
+                <img class="card-img-top" src="${producto.img}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p>$${producto.precio}</p>
+                    <p>Cantidad: ${producto.cantidad}</p>
+                    <button class="btn btn-danger" onClick="eliminarProductoDelCarrito(${id})">Eliminar</button>
+                </div>
+            </div>
+        </div>
+        `;
+  });
+
+  carritoHTML.innerHTML = html;
+
+  calcularTotal();
+}
+
+function calcularTotal() {
+  let total = 0;
+
+  carrito.forEach((producto) => {
+    total += producto.precio * producto.cantidad;
+  });
+
+  console.log(total);
+}
+// Funcion para eliminar productos del carrito
+const eliminarProductoDelCarrito = (id) => {
+  console.log(carrito[id].cantidad); 
+  carrito[id].cantidad--;
+  console.log(carrito[id].cantidad);
+
+  if (carrito[id].cantidad == 0) {
+    carrito.splice(id, 1);
+  }
+
+  renderizarCarrito();
+};
+
+
+
